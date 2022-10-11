33c35145fb49805538f39bfafe97cbefbd84e7b5
BT_3W2_13 <amirjonu@nycstudents.net>
2022-10-11 12:31:03 -0400

    added code

---
+++ src/LabRunner.java
@@ -0,0 +1,14 @@
+public class LabRunner {
+    public static void main(String[] args){
+        Student student = new Student("Abby", "Smith", 2023);
+        student.addTestScore(95);
+        student.addTestScore(98);
+        System.out.println(student.getStudentInfo());
+
+
+
+
+
+
+    }
+}
